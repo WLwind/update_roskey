9d5beae28670468584f591b7b883d6574669f843
WLwind <1121391435@qq.com>
2021-05-31 16:20:21 +0800

    add ros2 branch

--- update_roskey.sh
+++ update_roskey.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 git clone https://raw.githubusercontent.com/ros/rosdistro.git
 sudo cp rosdistro/ros.key /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://mirrors.tuna.tsinghua.edu.cn/ros/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros-latest.list > /dev/null
+sudo rm -f /etc/apt/sources.list.d/ros2*
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://mirrors.tuna.tsinghua.edu.cn/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list > /dev/null
