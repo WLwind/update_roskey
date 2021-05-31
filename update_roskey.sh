aa55935b2134421662203b860efd0cae9a54118e
WLwind <1121391435@qq.com>
2021-05-31 16:43:14 +0800

    fix the git clone link

--- update_roskey.sh
+++ update_roskey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-git clone https://raw.githubusercontent.com/ros/rosdistro.git
+git clone https://github.com/ros/rosdistro.git
 sudo cp rosdistro/ros.key /usr/share/keyrings/ros-archive-keyring.gpg
 sudo rm -f /etc/apt/sources.list.d/ros2*
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://mirrors.tuna.tsinghua.edu.cn/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list > /dev/null
