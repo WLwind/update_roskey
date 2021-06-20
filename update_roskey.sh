6fe147e44ff635309650f8b71faab0b063fa95d7
WLwind <1121391435@qq.com>
2021-06-20 13:01:55 +0800

    add offline key

--- update_roskey.sh
+++ update_roskey.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-git clone https://github.com/ros/rosdistro.git
-sudo cp rosdistro/ros.key /usr/share/keyrings/ros-archive-keyring.gpg
+sudo cp ros.key /usr/share/keyrings/ros-archive-keyring.gpg
 sudo rm -f /etc/apt/sources.list.d/ros2*
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://mirrors.tuna.tsinghua.edu.cn/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list > /dev/null
