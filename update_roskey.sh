3fe839ee130f7f19e5561d8abb7525dafc28ae1d
WLwind <1121391435@qq.com>
2022-07-06 11:07:44 +0800

    change mirror to ustc
    tsinghua mirror has some problems at this time

--- update_roskey.sh
+++ update_roskey.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 sudo cp ros.key /usr/share/keyrings/ros-archive-keyring.gpg
 sudo rm -f /etc/apt/sources.list.d/ros2*
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://mirrors.tuna.tsinghua.edu.cn/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://mirrors.ustc.edu.cn/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2-latest.list > /dev/null
